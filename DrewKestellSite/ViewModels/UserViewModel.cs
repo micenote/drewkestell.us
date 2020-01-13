92e20ee480dedc55b45d44346304e562d8696070
dkestell <drew.kestell@gmail.com>
2020-01-13 12:43:04 -0800

    WIP

---
+++ DrewKestellSite/ViewModels/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DrewKestellSite.Models;
+
+namespace DrewKestellSite.ViewModels
+{
+    public class UserViewModel
+    {
+        public UserViewModel(User user)
+        {
+            Id = user.Id;
+            Username = user.Username;
+        }
+
+        public int Id { get; }
+
+        public string Username { get; }
+    }
+}
